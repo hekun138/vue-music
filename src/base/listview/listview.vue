7f6e0d6f34b75acc5d385c0bbbf539942a1b3ee3
Hogan <418730859@qq.com>
2020-10-07 13:55:44 +0800

    完成listview

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,69 @@
+<template>
+  <scroll class="listview" :data="data">
+    <ul>
+      <li v-for="group in data" :key="group.title" class="list-group">
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li v-for="item in group.items" :key="item.id" class="list-group-item">
+            <img class="avatar" v-lazy="item.avatar"/>
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </scroll>
+</template>
+
+<script>
+import Scroll from '@/base/scroll/scroll'
+export default {
+  props: {
+    data: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  components: {
+    Scroll
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/common/scss/variable";
+.listview {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background: $color-background;
+  .list-group {
+    padding-bottom: 30px;
+    .list-group-title {
+      height: 30px;
+      line-height: 30px;
+      padding-left: 20px;
+      font-size: $font-size-small;
+      color: $color-text-l;
+      background: $color-highlight-background;
+    }
+    .list-group-item {
+      display: flex;
+      align-items: center;
+      padding: 20px 0 0 30px;
+      .avatar {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+      }
+      .name {
+        margin-left: 20px;
+        color: $color-text-l;
+        font-size: $font-size-medium;
+      }
+    }
+  }
+}
+</style>
