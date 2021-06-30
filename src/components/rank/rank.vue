7aaf18d6daa2aff3c5b509b90205630379424e39
Hogan <418730859@qq.com>
2021-07-01 00:20:48 +0800

    排行榜数据应用

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,9 +1,111 @@
 <template>
-  <div>
-    rank
+  <div class="rank" ref="rank">
+    <scroll :data="topList" class="toplist" ref="toplist">
+      <ul>
+        <li class="item" v-for="(item, index) in topList" :key="index">
+          <div class="icon">
+            <img width="100" height="100" v-lazy="item.picUrl">
+          </div>
+          <ul class="songlist">
+            <li class="song" v-for="(song, index) in item.songList" :key="index">
+              <span>{{index + 1}}</span>
+              <span>{{song.songname}} - {{song.singername}}</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <div class="loading-container" v-show="!topList.length">
+        <loading></loading>
+      </div>
+    </scroll>
   </div>
 </template>
 
-<script></script>
+<script type="text/ecmascript-6">
+import { getTopList } from '@/api/rank'
+import { ERR_OK } from '@/api/config'
+import Scroll from '@/base/scroll/scroll'
+import Loading from '@/base/loading/loading'
+import { playListMixin } from '@/common/js/mixin'
 
-<style></style>
+export default {
+  mixins: [playListMixin],
+  data () {
+    return {
+      topList: []
+    }
+  },
+  created () {
+    this._getTopList()
+  },
+  methods: {
+    handlePlaylist (playList) {
+      const bottom = playList.length ? '60px' : ''
+      this.$refs.rank.style.bottom = bottom
+      this.$refs.toplist.refresh()
+    },
+    _getTopList () {
+      getTopList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.topList = res.data.topList
+        }
+      })
+    }
+  },
+  components: {
+    Scroll,
+    Loading
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/common/scss/variable';
+@import '@/common/scss/mixin';
+
+.rank {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  .loading-container {
+    position: absolute;
+    width: 100%;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  .toplist {
+    height: 100%;
+    overflow: hidden;
+    .item {
+      display: flex;
+      margin: 0 20px;
+      padding-top: 20px;
+      &:last-child {
+        padding-bottom: 20px;
+      }
+      .icon {
+        flex: 0 0 100px;
+        width: 100px;
+        height: 100px;
+      }
+      .songlist {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        height: 100px;
+        overflow: hidden;
+        background: $color-highlight-background;
+        color: $color-text-d;
+        font-size: $font-size-small;
+        padding: 0 20px;
+        .song {
+          @include no-wrap();
+          line-height: 26px;
+        }
+      }
+    }
+  }
+}
+</style>
